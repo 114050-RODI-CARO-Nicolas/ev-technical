bcace37551dc8ed49374de7d261f989325991f3e
114050-RODI-CARO-Nicolas <113730734+114050-RODI-CARO-Nicolas@users.noreply.github.com>
2024-11-19 17:50:46 -0300

    WIP setting up main service class for frontend project

--- EvoltisTechnical_FE/src/core/services/programmer/programmer.service.ts
+++ EvoltisTechnical_FE/src/core/services/programmer/programmer.service.ts
@@ -4,14 +4,48 @@ import { ProgrammerResponseDTO } from '../../../app/features/programmer/models/p
 import { ProgrammerDetailDTO } from '../../../app/features/programmer/models/programmer-detail-dto';
 import { Skill } from '../../../app/features/programmer/models/skill';
 import { HttpClient } from '@angular/common/http';
-
+import { environment } from '../../../environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgrammerService {
 
-  private httpClient: HttpClient = inject(HttpClient)
+  private baseUrl = environment.apiUrlRecruiting;
+  private http: HttpClient = inject(HttpClient);
+
+
+    getAllProgrammers(): Observable<ProgrammerResponseDTO[]> {
+      return this.http.get<ProgrammerResponseDTO[]>(`${this.baseUrl}/programmers`);
+    }
+  
+
+    getProgrammerById(id: number): Observable<ProgrammerDetailDTO> {
+      return this.http.get<ProgrammerDetailDTO>(`${this.baseUrl}/programmers/${id}`);
+    }
+  
+
+    getProgrammerByEmail(email: string): Observable<ProgrammerDetailDTO> {
+      return this.http.get<ProgrammerDetailDTO>(`${this.baseUrl}/programmers/by-email/${email}`);
+    }
+  
+
+    createProgrammer(programmer: CreateProgrammerDTO): Observable<ProgrammerDetailDTO> {
+      return this.http.post<ProgrammerDetailDTO>(`${this.baseUrl}/programmers`, programmer);
+    }
+  
+
+    updateProgrammer(id: number, programmer: Partial<CreateProgrammerDTO>): Observable<ProgrammerDetailDTO> {
+      return this.http.put<ProgrammerDetailDTO>(`${this.baseUrl}/programmers/${id}`, programmer);
+    }
+  
+
+    deleteProgrammer(id: number): Observable<ProgrammerDetailDTO> {
+      return this.http.delete<ProgrammerDetailDTO>(`${this.baseUrl}/programmers/${id}`);
+    }
+
+  
 
   
 }
--- EvoltisTechnical_FE/src/environment.ts
+++ EvoltisTechnical_FE/src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api/'
+    apiUrlRecruiting: 'http://localhost:5000/api/recruiting'
 }
