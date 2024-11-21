abc0b28aa9612ef42b6481fdf7ee59dae755f924
114050-RODI-CARO-Nicolas <113730734+114050-RODI-CARO-Nicolas@users.noreply.github.com>
2024-11-21 14:01:21 -0300

    most recent migration: removed IsActive property from ProgrammerEntity as it already exists in BaseEntitiy

--- EvoltisTechnical_BE/EvoltisTechnical_BE/Migrations/20241121165727_EntityAdjustmentRemovedActiveProperty.Designer.cs
+++ EvoltisTechnical_BE/EvoltisTechnical_BE/Migrations/20241121165727_EntityAdjustmentRemovedActiveProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EvoltisTechnical_BE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EvoltisTechnical_BE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241121165727_EntityAdjustmentRemovedActiveProperty")]
+    partial class EntityAdjustmentRemovedActiveProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EvoltisTechnical_BE/EvoltisTechnical_BE/Migrations/20241121165727_EntityAdjustmentRemovedActiveProperty.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvoltisTechnical_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntityAdjustmentRemovedActiveProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9610), new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9627) });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9628), new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9629) });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9630), new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9630) });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9631), new DateTime(2024, 11, 21, 13, 57, 27, 81, DateTimeKind.Local).AddTicks(9631) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4364), new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4383) });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4386), new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4386) });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4387), new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4388) });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedAt", "LastUpdatedAt" },
+                values: new object[] { new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4389), new DateTime(2024, 11, 19, 0, 14, 56, 246, DateTimeKind.Local).AddTicks(4389) });
+        }
+    }
+}
